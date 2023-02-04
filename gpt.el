d9ac3246b42d474b42ab93f793dd7bd1260f77c9
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-02-04 19:53:39 +0100

    full rewrite & added history

--- gpt.el
+++ gpt.el
@@ -23,51 +23,82 @@
 ;;; Imports
 (require 'json)
 (require 'url)
+
 ;;; Group
 (defgroup gpt nil
   "Setting the prefix."
   :group 'tools
   :prefix "gpt-")
+
 ;;; Functions
-(defun gpt-make-web-request (prompt)
-  "Do web request to the ChatGPT \
-@Param: PROMPT"
-  (let ((url-request-method "POST")
-        (url-request-extra-headers
-         `(("Content-Type" . "application/json")
-           ("Authorization" . ,(concat "Bearer " (getenv "OPENAI_API_KEY")))))
-        (url-request-data
-         (json-encode
-          `(("model" . "text-davinci-003")
-            ("prompt" . ,(encode-coding-string prompt 'utf-8))
-            ("max_tokens" . 2000)
-            ("temperature" . 0.7)))))
-    (with-current-buffer (url-retrieve-synchronously "https://api.openai.com/v1/completions")
-      (goto-char (point-min))
-      (search-forward "\n\n")
-      (let ((response (json-read)))
-        (let ((text (decode-coding-string (cdr (assoc 'text (elt (cdr (assoc 'choices response)) 0))) 'cp1252)))
-          (kill-buffer (current-buffer))
-          (set-buffer (get-buffer-create "ChatGPT Query Result"))
-          (erase-buffer)
-          (insert text)
-          (visual-line-mode 1)
-          (display-buffer-below-selected (current-buffer) '(nil (window-height . fit-window-to-buffer)))
-          (select-window (get-buffer-window (current-buffer))))
-        (let ((finish-reason (cdr (assoc 'finish_reason (elt (cdr (assoc 'choices response)) 0)))))
-          (when (not (string= finish-reason "stop"))
-            (message "Completion stopped for reason: %s" finish-reason)))))))
+(defun gpt--write-to-history-file (prompt result)
+  "Write the PROMPT and RESULT to the history file."
+  (let ((history-file-path (concat user-emacs-directory "gpt-history")))
+    (unless (file-exists-p history-file-path)
+      (write-region "" nil history-file-path))
+    (with-temp-buffer
+      (insert (format "%s\n%s\n\n===============\n" prompt result))
+      (append-to-file (point-min) (point-max) history-file-path))))
 
-(defun gpt-read-prompt-from-minibuffer ()
-  "Read a prompt string from the minibuffer."
-  (read-from-minibuffer "Enter prompt: "))
-
-(defun gpt-query-chatgpt ()
-  "Query ChatGPT."
+(defun gpt-clear-history ()
+  "Clear the contens of the history file."
   (interactive)
-  (gpt-make-web-request (gpt-read-prompt-from-minibuffer)))
+  (let ((history-file-path (concat user-emacs-directory "gpt-history")))
+    (when (file-exists-p history-file-path)
+      (write-region "" nil history-file-path)))
+  (message "Cleared ChatGPT history"))
 
-(gpt-query-chatgpt)
+(defun gpt-view-history ()
+  "Displays the contents of the GPT history file in a buffer."
+  (interactive)
+  (let ((history-file (concat user-emacs-directory "gpt-history")))
+    (with-current-buffer (get-buffer-create "GPT Query History")
+      (set-buffer-file-coding-system 'windows-1252)
+      (erase-buffer)
+      (insert-file-contents history-file)
+      (visual-line-mode 1)
+      (display-buffer (current-buffer) '(nil (window-height . fit-window-to-buffer))))))
+
+(defun gpt--make-web-request (prompt)
+  "Do web request to the ChatGPT.
+@Param: PROMPT"
+  (if (not (getenv "OPENAI_API_KEY"))
+      (error "Please export the OPENAI_API_KEY")
+    (if (string= prompt "")
+        (error "Prompt cannot be empty")
+      (let* ((url-request-method "POST")
+             (url-request-extra-headers
+              `(("Content-Type" . "application/json")
+                ("Authorization" . ,(concat "Bearer " (getenv "OPENAI_API_KEY")))))
+             (url-request-data
+              (json-encode
+               `(("model" . "text-davinci-003")
+                 ("prompt" . ,(encode-coding-string prompt 'utf-8))
+                 ("max_tokens" . 2000)
+                 ("temperature" . 0.7))))
+             (response-buffer (url-retrieve-synchronously "https://api.openai.com/v1/completions"))
+             (response (with-current-buffer response-buffer
+                         (goto-char (point-min))
+                         (search-forward "\n\n")
+                         (json-read))))
+        (with-current-buffer (get-buffer-create "ChatGPT Result")
+          (set-buffer-file-coding-system 'windows-1252)
+          (erase-buffer)
+          (insert (cdr (assoc 'text (elt (cdr (assoc 'choices response)) 0))))
+          (visual-line-mode 1)
+          (display-buffer (current-buffer) '(nil (window-height . fit-window-to-buffer))))
+        (gpt--write-to-history-file prompt (cdr (assoc 'text (elt (cdr (assoc 'choices response)) 0))))
+        (kill-buffer response-buffer)))))
+
+(defun gpt-query ()
+  "Queries ChatGPT with a prompt provide by the minibuffer."
+  (interactive)
+  (let ((prompt (read-from-minibuffer "Enter prompt: ")))
+    (if (string= prompt "")
+        (message "Prompt cannot be empty")
+      (gpt--make-web-request prompt))))
+
+(gpt-query)
 
 ;;; Export
 (provide 'gpt)
